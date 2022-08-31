6704062aa882a6a0b7905e4ab01c8539c2ac36c2
Rayncor <sowa332@gmail.com>
2022-08-31 11:27:19 -0700

    stuff

--- server.js
+++ server.js
@@ -1,8 +1,6 @@
 const express = require('express');
 const routes = require('./controllers');
-const app = express();
-const PORT = process.env.PORT || 3001;
-// const cors = require('cors')
+const cors = require('cors')
 // const authConfig = require("../auth_config.json");
 // const { expressjwt: jwt } = require("express-jwt");
 // const jwksRsa = require("jwks-rsa");
@@ -12,10 +10,12 @@ const sequelize = require('./config/connection');
 
 
 //GMS DO NOT MOVE APP.USE CORS OR EXPRESS FROM THEIR POSITIONING!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-// app.use(cors());
 
 
+const app = express();
+const PORT = process.env.PORT || 3001;
 app.use(express.json());
+app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 app.use(routes);
 
