2682cfdedc9d83e0fcb6a270f5a9c306ba48a023
Rayncor <sowa332@gmail.com>
2022-09-01 21:49:07 -0700

    its working

--- controllers/api/coachRoutes.js
+++ controllers/api/coachRoutes.js
@@ -1,5 +1,7 @@
 const router = require('express').Router();
 const { Client, Coach, } = require('../../models');
+const bcrypt = require('bcrypt');
+const jwt = require("jsonwebtoken");
 
 
 //TODO: WORKING
@@ -33,53 +35,124 @@ router.get("/:id", async (req, res) => {
   }
 });
 
+
+
+//GMS figure out env
+//GMS how are username and password passed
+router.post("/", (req, res) => {
+  Coach.create(req.body).then(newCoach => {
+    const token = jwt.sign({
+      id: newCoach.id,
+      email: newCoach.email
+    }, process.env.JWT_SECRET, {
+      expiresIn: "2h"
+    })
+    return res.json({
+      token: token,
+      user: newCoach
+    })
+  }).catch(err => {
+    res.status(500).json({ msg: "Sorry, we couldn't process your request!", err })
+  })
+})
+
+
+router.post("/login", (req, res) => [
+  Coach.findOne({
+    where: { email: req.body.email }
+  }).then(foundUser => {
+    if (!foundUser) {
+      return res.status(401).json({ msg: "Coach does not exist" })
+    }
+    else if (!bcrypt.compareSync(req.body.password, foundUser.password)) {
+      return res.status(401).json({ msg: "invalid login password" })
+    } else {
+      const token = jwt.sign({
+        id: foundUser.id,
+        email: foundUser.email
+      }, process.env.JWT_SECRET, {
+        expiresIn: "2hr"
+      })
+      return res.json({ token: token, user: foundUser })
+    }
+  }).catch(err => {
+    res.status(500).json({ msg: "Server error", err })
+  })
+])
+
+router.get("/user-from-token", (req, res) => {
+  const token = req.headers.authorization.split(" ")[1]
+  try {
+    const userData = jwt.verify(token, process.env.JWT_SECRET)
+    Coach.findByPk(userData.id, {
+      include: {
+        model: Client
+      }
+    }).then(userData => {
+      res.json(userData)
+    }).catch(err => {
+      res.status(500).json({ msg: "an error occurred", err })
+    })
+  } catch {
+    res.status(403).json({ msg: "invalid token" })
+  }
+})
+
+
+
+
+
+//--------------------------------------------------------------------------------------------------------------------
+
+
+
 //TODO: WORKING
 //CREATES Coach
-router.post('/', async (req, res) => {
-  try {
-    const newCoach = await Coach.create({
-      username: req.body.username,
-      email: req.body.email,
-      password: req.body.password
-    });
+// router.post('/', async (req, res) => {
+//   try {
+//     const newCoach = await Coach.create({
+//       username: req.body.username,
+//       email: req.body.email,
+//       password: req.body.password
+//     });
 
-    res.status(200).json(newCoach);
-  } catch (err) {
-    res.status(400).json(err);
-  }
-});
+//     res.status(200).json(newCoach);
+//   } catch (err) {
+//     res.status(400).json(err);
+//   }
+// });
 
 
-//TODO: WORKING
-//GMS LOGIN coach
-router.post('/login', async (req, res) => {
-  try {
-    const existingUserData = await Coach.findOne({ where: { email: req.body.email } });
-    if (!existingUserData) {
-      res
-        .status(400)
-        .json({ message: 'No coach was found with that email.' });
-      return;
-    }
-    const validPassword = await existingUserData.checkPassword(req.body.password);
-    if (!validPassword) {
-      res
-        .status(400)
-        .json({ message: 'Incorrect email or password, please try again' });
-      return;
-    }
-    req.session.save(() => {
-      req.session.coachId = existingUserData.id;
-      req.session.username = existingUserData.username;
-      req.session.logged_in = true;
-      console.log(existingUserData.id);
-      console.log(req.session.logged_in);
-      res.json({ coach: existingUserData, message: 'Coach successfully logged in.' });
-    });
-  } catch (err) {
-    res.status(400).json(err);
-  }
-});
+// //TODO: WORKING
+// //GMS LOGIN coach
+// router.post('/login', async (req, res) => {
+//   try {
+//     const existingUserData = await Coach.findOne({ where: { email: req.body.email } });
+//     if (!existingUserData) {
+//       res
+//         .status(400)
+//         .json({ message: 'No coach was found with that email.' });
+//       return;
+//     }
+//     const validPassword = await existingUserData.checkPassword(req.body.password);
+//     if (!validPassword) {
+//       res
+//         .status(400)
+//         .json({ message: 'Incorrect email or password, please try again' });
+//       return;
+//     }
+//     req.session.save(() => {
+//       req.session.coachId = existingUserData.id;
+//       req.session.username = existingUserData.username;
+//       req.session.logged_in = true;
+//       console.log(existingUserData.id);
+//       console.log(req.session.logged_in);
+//       res.json({ coach: existingUserData, message: 'Coach successfully logged in.' });
+//     });
+//   } catch (err) {
+//     res.status(400).json(err);
+//   }
+// });
 
 
 
